746d3ee9ece037e16bdabb7d5af07ebb6c699353
haeramkeem <ewqdsacxz2345@gmail.com>
2023-08-29 08:52:10 +0900

    docs: add algorithm content

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,7 +8,7 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `jouranl.haeramk.im`,
+    siteTitle: `journal.haeramk.im`,
     siteTitleAlt: `Journal | HaeramKim`,
     siteHeadline: `journal.haeramk.im`,
     siteUrl: `https://journal.haeramk.im`,
